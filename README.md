49b3c92e1fdc72fa7a80bed48524a7aa882a7d1a
Alex van den Hoogen <alex3305@gmail.com>
2018-11-30 22:05:08 +0100

    Initial commit
    Added custom versions of the pytradfri and tradfri components for testing
    only. These should fix the issues that were present with the pytradfri's
    observations failing after a while.

---
+++ README.md
@@ -0,0 +1,18 @@
+# Home Assistant - custom components (debug)
+
+This repository holds my debug versions of some custom components that I am
+working on. I cannot guarantee that these versions are up to date in any way.
+Also most of this code are licensed by their respectful owners. These 
+modifications are just for testing and should not be used.
+
+## Usage
+
+You can place these components in the `custom_components` directory of Home
+Asisstant. After a restart they should automatically be picked up. For easy
+access, you can download a ZIP file from this repo for testing.
+
+## Sources
+
+ * `tradfri` - https://github.com/home-assistant/home-assistant
+ * `pytradfri` - https://github.com/ggravlingen/pytradfri
+
---
+++ pytradfri/error.py
@@ -0,0 +1,46 @@
+"""Errors for PyTradfri."""
+
+
+class PytradfriError(Exception):
+    """Base Error"""
+    pass
+
+
+class RequestError(PytradfriError):
+    """An error happened sending or receiving a command."""
+    pass
+
+
+class ColorError(PytradfriError):
+    """An error happened matching color name."""
+    pass
+
+
+class RequestTimeout(RequestError):
+    """Error when sending or receiving the command timed out."""
+    pass
+
+
+class ClientError(RequestError):
+    """Error when the client caused the error.
+
+    See section 5.9.2 of draft-ietf-core-coap-04.
+    """
+    pass
+
+
+class ServerError(RequestError):
+    """Error when the server caused the error.
+
+    See section 5.9.3 of draft-ietf-core-coap-04.
+    """
+    pass
+
+
+class ObservationError(PytradfriError):
+    """Error when the observation got stuck.
+
+    This can happen with indefinate observations which can get stuck after
+    some time.
+    """
+    pass
